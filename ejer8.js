7916172ef9d92af5444c671a7266aa8a9fe46e69
JSBHA <jhoan-071995@hotmail.com>
2025-01-28 16:21:13 -0500

    Challenge 8 is completed where palindrome words are identified and a character is eliminated

---
+++ ejer8.js
@@ -0,0 +1,15 @@
+function checkPart(part) {
+    // Verificar si la cadena ya es un palíndromo
+    if (part === [...part].reverse().join('')) return true;
+
+    // Usar find() para intentar eliminar un solo carácter
+    return [...part].find((char, index) =>{
+        let newPart = part.slice(0, index) + part.slice(index + 1);
+        return newPart === [...newPart].reverse().join('');
+    })!== undefined;
+
+  }
+
+  console.log(checkPart("uwu"));    
+  console.log(checkPart("miidim")); 
+  console.log(checkPart("midu"));   
