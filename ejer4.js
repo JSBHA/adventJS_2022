2dee989cafdfadc3ab61bce8296fa1110c678d5a
JSBHA <jhoan-071995@hotmail.com>
2025-01-29 14:28:04 -0500

    Challenge 4 completed, we create a function that helps us put a box inside another box

--- ejer4.js
+++ ejer4.js
@@ -1,20 +1,7 @@
-const boxes = [
-    { l: 1, w: 1, h: 1 },
-    { l: 2, w: 2, h: 2 },
-    { l: 3, w: 1, h: 3 }
-  ]
-
-
-const boxes1 = [
-    { l: 1, w: 1, h: 1 },
-    { l: 3, w: 3, h: 3 },
-    { l: 2, w: 2, h: 2 }
-  ]
-
 function fitsInOneBox(boxes) {  
     
      // Ordenar las cajas por volumen (l * w * h)
-     boxes.sort((a, b) => (a.l * a.w * a.h)- (b.l * b.w * b.h));
+     boxes.sort((a, b) => (a.l * a.w * a.h) - (b.l * b.w * b.h));
 
      // Comprobar si cada caja puede caber en la siguiente
      for (let i = 1; i < boxes.length; i++){
@@ -30,3 +17,16 @@ function fitsInOneBox(boxes) {
      return true
   
   }
+
+  const boxes = [
+    { l: 1, w: 1, h: 1 },
+    { l: 2, w: 2, h: 2 },
+    { l: 3, w: 1, h: 3 }
+  ]
+
+
+const boxes1 = [
+    { l: 1, w: 1, h: 1 },
+    { l: 3, w: 3, h: 3 },
+    { l: 2, w: 2, h: 2 }
+  ]
