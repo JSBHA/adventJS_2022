b67155a0cfa69a36216f87867e8a19356089a56f
JSBHA <jhoan-071995@hotmail.com>
2025-02-04 13:54:49 -0500

    Challenge 4 completed, we create a function that helps us put a box inside another box

--- ejer4.js
+++ ejer4.js
@@ -1,32 +1,29 @@
-function fitsInOneBox(boxes) {  
-    
-     // Ordenar las cajas por volumen (l * w * h)
-     boxes.sort((a, b) => (a.l * a.w * a.h) - (b.l * b.w * b.h));
+function fitsInOneBox(boxes) {
+  // Ordenar las cajas por volumen (l * w * h)
+  boxes.sort((a, b) => a.l * a.w * a.h - b.l * b.w * b.h);
 
-     // Comprobar si cada caja puede caber en la siguiente
-     for (let i = 1; i < boxes.length; i++){
-       const box = boxes[i - 1];
-       const box1 = boxes[i];
+  // Comprobar si cada caja puede caber en la siguiente
+  for (let i = 1; i < boxes.length; i++) {
+    const box = boxes[i - 1];
+    const box1 = boxes[i];
 
-        // Verificar si la caja 1 cabe en la caja 2
-        if (!(box.l < box1.l && box.w < box1.w && box.h < box1.h)){
-         return false  // Si no cabe, devolvemos false
-       }
-     };         
-   
-     return true
-  
+    // Verificar si la caja 1 cabe en la caja 2
+    if (!(box.l < box1.l && box.w < box1.w && box.h < box1.h)) {
+      return false; // Si no cabe, devolvemos false
+    }
   }
 
-  const boxes = [
-    { l: 1, w: 1, h: 1 },
-    { l: 2, w: 2, h: 2 },
-    { l: 3, w: 1, h: 3 }
-  ]
+  return true;
+}
 
+const boxes = [
+  { l: 1, w: 1, h: 1 },
+  { l: 2, w: 2, h: 2 },
+  { l: 3, w: 1, h: 3 },
+];
 
 const boxes1 = [
-    { l: 1, w: 1, h: 1 },
-    { l: 3, w: 3, h: 3 },
-    { l: 2, w: 2, h: 2 }
-  ]
+  { l: 1, w: 1, h: 1 },
+  { l: 3, w: 3, h: 3 },
+  { l: 2, w: 2, h: 2 },
+];
