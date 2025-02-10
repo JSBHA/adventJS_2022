45bb896051a298d713ab35f118e682cad5365691
JSBHA <jhoan-071995@hotmail.com>
2025-02-09 19:38:50 -0500

    Challenge 6 is completed where you must create an ornament to generate the correct number of rows for the 3D cube

--- ejer6.js
+++ ejer6.js
@@ -1,26 +1,24 @@
 function createCube(size) {
+  //La parte superior (head)
+  let head = [];
+  //La parte inferior (foot)
+  let foot = [];
 
-    //La parte superior (head)
-    let head = [];
-    //La parte inferior (foot)
-    let foot= [];
-    
-    // Cambiar el bucle para que vaya hasta 'size' inclusive
-    for (let i = 1; i <= size; i++){
-         // Parte superior: corregir la construcción del patrón
-        head.push(' '.repeat(size - i) + '/\\'.repeat(i) + '_\\'.repeat(size));
+  // Cambiar el bucle para que vaya hasta 'size' inclusive
+  for (let i = 1; i <= size; i++) {
+    // Parte superior: corregir la construcción del patrón
+    head.push(" ".repeat(size - i) + "/\\".repeat(i) + "_\\".repeat(size));
 
-        // Parte inferior: corregir la construcción del patrón
-        foot.push(' '.repeat(i - 1) + '\\/'.repeat(size - (i - 1)) + '_/'.repeat(size));
-    }
-
-    // Concatenamos ambas partes y las unimos con saltos de línea
-    return [...head, ...foot].join('\n');
-
+    // Parte inferior: corregir la construcción del patrón
+    foot.push(
+      " ".repeat(i - 1) + "\\/".repeat(size - (i - 1)) + "_/".repeat(size)
+    );
   }
-  
-  console.log(createCube(3));
-  console.log(createCube(2));
-  console.log(createCube(1));
- 
 
+  // Concatenamos ambas partes y las unimos con saltos de línea
+  return [...head, ...foot].join("\n");
+}
+
+console.log(createCube(3));
+console.log(createCube(2));
+console.log(createCube(1));
