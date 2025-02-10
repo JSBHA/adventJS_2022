7c5d35507971143bfe64a24c74c7b623ecd5a218
JSBHA <jhoan-071995@hotmail.com>
2025-02-09 19:33:26 -0500

    Challenge 1 is completed by creating a function that packs the gifts

--- ejer1.js
+++ ejer1.js
@@ -1,17 +1,14 @@
 function wrapping(gifts) {
+  // Se crea el nuevo arr dodne esten empacos los regalos
+  return gifts.map((gift) => {
+    //Calcular la longitud del empaque
+    let stars = "*".repeat(gift.length * 2);
+    //Se empaca el regalo utilizando el salto entre lianes /n
+    return `${stars}\n*${gift}*\n${stars}`;
+  });
+}
 
-  // Se crea el nuevo arr dodne esten empacos los regalos 
-    return gifts.map(gift => {
-      //Calcular la longitud del empaque 
-      let stars = '*'.repeat(gift.length * 2 );
-      //Se empaca el regalo utilizando el salto entre lianes /n 
-      return  `${stars}\n*${gift}*\n${stars}`;
-    })
+const gifts = ["cat", "game", "socks"];
+const wrapped = wrapping(gifts);
 
-  }
-
-
-  const gifts = ['cat', 'game', 'socks']
-  const wrapped = wrapping(gifts)
-
-console.log(wrapped)
+console.log(wrapped);
