1021e21e8cc40c35ec6e4570a747096f65386360
JSBHA <jhoan-071995@hotmail.com>
2025-02-09 19:35:11 -0500

    Challenge 2 completed, we create a function that tells us how many overtime hours you must work to start the holiday.

--- ejer2.js
+++ ejer2.js
@@ -1,24 +1,22 @@
 function countHours(year, holidays) {
-  let totalHorasExtras = 0 ;
+  let totalHorasExtras = 0;
 
   //Recorrer los dias festivos
-  holidays.forEach(holiday => {
+  holidays.forEach((holiday) => {
     // Crear una fecha a partir del año y el día festivo
-    const date = new Date (`${year}-${holiday}`);
+    const date = new Date(`${year}-${holiday}`);
 
-     // Obtener el día de la semana: 0 es domingo, 1 es lunes
+    // Obtener el día de la semana: 0 es domingo, 1 es lunes
     const diaSemana = date.getDay();
 
     //Si el día festivo es de lunes a viernes, contar 2 horas extra
-    if (diaSemana >= 1 && diaSemana <= 5){
-       totalHorasExtras += 2 ;
+    if (diaSemana >= 1 && diaSemana <= 5) {
+      totalHorasExtras += 2;
     }
   });
-    return  totalHorasExtras
-   
-  }
+  return totalHorasExtras;
+}
 
-  console.log(countHours(year, holidays))
-  const year = 2022 ;
-  const holidays = ['01/06', '04/01', '12/25'];
-  
+console.log(countHours(year, holidays));
+const year = 2022;
+const holidays = ["01/06", "04/01", "12/25"];
