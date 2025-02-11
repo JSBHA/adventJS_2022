2533b4334fc18dc1105f406e9b123839a247d309
JSBHA <jhoan-071995@hotmail.com>
2025-02-10 22:46:54 -0500

    Challenge 8 is completed where palindrome words are identified and a character is eliminated

--- ejer9.js
+++ ejer9.js
@@ -1,15 +1,14 @@
 function countTime(leds) {
-    // Convertimos el array en una cadena y luego lo dividimos por cada '1'
-    let led = leds.join("").split("1")
+  // Convertimos el array en una cadena y luego lo dividimos por cada '1'
+  let led = leds.join("").split("1");
 
-    // Unimos el primer grupo de ceros con el último para simular el ciclo de LEDs
-    led[0] += led.pop();
+  // Unimos el primer grupo de ceros con el último para simular el ciclo de LEDs
+  led[0] += led.pop();
 
-     // Calculamos el tamaño del grupo más largo de ceros consecutivos
-    return Math.max(...led.map(ledOn => ledOn.length)) * 7;
+  // Calculamos el tamaño del grupo más largo de ceros consecutivos
+  return Math.max(...led.map((ledOn) => ledOn.length)) * 7;
+}
 
-  }
-
-  console.log(countTime([0, 1, 1, 0, 1]));
-  console.log(countTime([0, 0, 0, 1]));
-  console.log(countTime([0, 0, 1, 0, 0]));
+console.log(countTime([0, 1, 1, 0, 1]));
+console.log(countTime([0, 0, 0, 1]));
+console.log(countTime([0, 0, 1, 0, 0]));
